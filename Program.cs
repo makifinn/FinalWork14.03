da5f9f229419945a06660dd47f534ca259e452f7
Maki <kononenkotochka@gmail.com>
2023-03-13 22:45:58 +0500

    added ReadInt method

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿bool IsWork = true;
 
-while(IsWork)
+while (IsWork)
 {
     Console.WriteLine("Программа принимает на вход массив указанного размера и на выходе создаёт массив содержащий все элементы первого массива меньше 3х символов");
     Console.WriteLine();
@@ -12,15 +12,26 @@ while(IsWork)
         switch (i)
         {
             case 1:
-            {
-                Task_1();
-
-                void Task_1()
                 {
-                    
+                    Task_1();
+
+                    void Task_1()
+                    {
+                        int ReadInt(string argument)
+                        {
+                            Console.Write($"{argument}");
+                            int number;
+
+                            while (!int.TryParse(Console.ReadLine(), out number))
+                            {
+                                Console.WriteLine("вы ввели не число");
+                            }
+
+                            return number;
+                        }
+                    }
+                    break;
                 }
-                break;
-            }
 
             case -1: IsWork = false; break;
         }
