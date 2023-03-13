fe45cd576196eb1a9f45b39ce6b8b2efeb20e247
Maki <kononenkotochka@gmail.com>
2023-03-13 22:44:27 +0500

    added structure of programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsWork = true;
+
+while(IsWork)
+{
+    Console.WriteLine("Программа принимает на вход массив указанного размера и на выходе создаёт массив содержащий все элементы первого массива меньше 3х символов");
+    Console.WriteLine();
+    Console.WriteLine("Для начала работы введите 1");
+    Console.WriteLine("Для завершения работы введите -1");
+
+    if (int.TryParse(Console.ReadLine(), out int i))
+    {
+        switch (i)
+        {
+            case 1:
+            {
+                Task_1();
+
+                void Task_1()
+                {
+                    
+                }
+                break;
+            }
+
+            case -1: IsWork = false; break;
+        }
+    }
+}
